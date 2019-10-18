7bef4c637082b1117128cfc9fa44c12aea9b454d
pradnya shelke <pradnyashelke21@gmail.com>
2019-10-18 13:54:56 -0700

    initial commit via eclipse

---
+++ src/practice/Student.java
@@ -0,0 +1,61 @@
+package practice;
+
+public class Student
+{
+    private int id;
+    private String stName;
+    private int stclass;
+    private double percentage;
+
+    public Student(){
+
+    }
+    public Student(int id, String stName) {
+        this.id = id;
+        this.stName = stName;
+    }
+   
+    public Student(int id, String stName, int stclass , Double percentage) {
+        this.id = id;
+        this.stName = stName;
+        this.stclass = stclass;
+        this.percentage = percentage;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getStName() {
+        return stName;
+    }
+
+    public void setStName(String stName) {
+        this.stName = stName;
+    }
+
+    public int getStclass() {
+        return stclass;
+    }
+
+    public void setStclass(int stclass) {
+        this.stclass = stclass;
+    }
+	public double getPercentage() {
+		return percentage;
+	}
+	public void setPercentage(double percentage) {
+		this.percentage = percentage;
+	}
+
+    
+    
+  
+
+    
+
+    
+}
