bf5b1f1495891eb63fd957414ca2b3a31709bcc7
pradnya shelke <pradnyashelke21@gmail.com>
2019-10-18 14:11:01 -0700

    readingFile

---
+++ src/practice/Readingfile.java
@@ -0,0 +1,27 @@
+package practice;
+
+import java.io.BufferedReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
+public class Readingfile {
+
+    public static void main(String[]arg){
+        StringBuilder sb = new StringBuilder();
+       
+  try {
+    BufferedReader bReader = Files.newBufferedReader(Paths.get("info"));
+    
+    String line;
+    while((line=bReader.readLine())!=null)
+       sb.append(line).append("\t");
+
+   System.out.println(sb);
+  }
+   catch (Exception e) {
+    System.out.println(e);
+  }
+       
+}
+}
